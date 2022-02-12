2bf7e5e2dc98caacbdddc87eb368115aed668d7a
ksquarre <kahkashakhan6799@gmail.com>
2022-02-12 22:22:03 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const btn1 = document.querySelector('#click')
+const btn2 = document.querySelector('#click2')
+
+const menu =document.querySelector('.list')
+btn1.addEventListener('click',(e)=>{
+
+    menu.style.right='0'
+   
+})
+btn2.addEventListener('click',(e)=>{
+
+    menu.style.right='-200px'
+   
+})
